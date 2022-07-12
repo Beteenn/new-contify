e24d2b345166aefeeae40fcb9da929385168b151
Gilberto Barbosa <gilberto.barbosa@7comm.com.br>
2022-07-12 16:28:13 -0300

    Ajuste nomes variaveis

--- Rentfy.Application/Services/PhysicalPersonAppService.cs
+++ Rentfy.Application/Services/PhysicalPersonAppService.cs
@@ -19,14 +19,38 @@ namespace Rentfy.Application.Services
             _physicalPersonService = physicalPersonService ?? throw new ArgumentNullException(nameof(physicalPersonService));
         }
 
+        public async Task<Result<PhysicalPersonViewModel>> GetPhysicalPerson(long id)
+        {
+            var personResult = await _physicalPersonService.GetById(id);
+
+            return Mapper.Map<Result<PhysicalPersonViewModel>>(personResult);
+        }
+
         public async Task<Result<PhysicalPersonViewModel>> CreatePhysicalPerson(PhysicalPersonDto personDto)
         {
             var person = new PhysicalPerson(personDto.Name, personDto.Email, personDto.LastName,
                 personDto.BirthDate, personDto.Document);
 
-            var personUpdated = await _physicalPersonService.CreatePhysicalPerson(person);
+            var personResult = await _physicalPersonService.CreatePhysicalPerson(person);
 
-            return Mapper.Map<Result<PhysicalPersonViewModel>>(personUpdated);
+            return Mapper.Map<Result<PhysicalPersonViewModel>>(personResult);
+        }
+
+        public async Task<Result<PhysicalPersonViewModel>> UpdatePhysicalPerson(PhysicalPersonDto personDto)
+        {
+            var personUpdate = new PhysicalPerson(personDto.Name, personDto.LastName, personDto.Email,
+                personDto.BirthDate, personDto.Document);
+
+            var personResult = await _physicalPersonService.Update(personDto.Id, personUpdate);
+
+            return Mapper.Map<Result<PhysicalPersonViewModel>>(personResult);
+        }
+
+        public async Task<Result> DeletePhysicalPersonById(long id)
+        {
+            var result = await _physicalPersonService.DeleteById(id);
+
+            return Mapper.Map<Result>(result);
         }
     }
 }
