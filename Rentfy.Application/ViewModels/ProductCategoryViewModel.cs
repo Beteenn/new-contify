e9b0ade8cc18c02c193ffe86bc08520f83052f20
Gilberto Barbosa <gilberto.barbosa@7comm.com.br>
2022-07-13 12:27:29 -0300

    [Categoria]: ViewModel e Dto criados

---
+++ Rentfy.Application/ViewModels/ProductCategoryViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Rentfy.Application.ViewModels
+{
+    public class ProductCategoryViewModel
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+    }
+}
