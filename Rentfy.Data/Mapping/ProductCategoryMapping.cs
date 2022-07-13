881dcef9497c68602424d1cafff7ffa033a260bc
Gilberto Barbosa <gilberto.barbosa@7comm.com.br>
2022-07-13 12:27:01 -0300

    [Categoria]: Entidade, Mapping, Migration criados

---
+++ Rentfy.Data/Mapping/ProductCategoryMapping.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Rentfy.Domain.Entities;
+
+namespace Rentfy.Data.Mapping
+{
+    public class ProductCategoryMapping : IEntityTypeConfiguration<ProductCategory>
+    {
+        public void Configure(EntityTypeBuilder<ProductCategory> builder)
+        {
+            builder.ToTable("ProductCategory");
+
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Name)
+                .HasMaxLength(120)
+                .IsRequired();
+        }
+    }
+}
--- Rentfy.Data/Migrations/20220713152357_Add-ProductCategory.Designer.cs
+++ Rentfy.Data/Migrations/20220713152357_Add-ProductCategory.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Rentfy.Data.Configuration;
 
 namespace Rentfy.Data.Migrations
 {
     [DbContext(typeof(RentfyContext))]
-    partial class RentfyContextModelSnapshot : ModelSnapshot
+    [Migration("20220713152357_Add-ProductCategory")]
+    partial class AddProductCategory
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
