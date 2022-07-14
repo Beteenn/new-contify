4ef67e9e3a3566102b87c1a5e79c333d2ccf171c
Gilberto Barbosa <gilberto.barbosa@7comm.com.br>
2022-07-14 15:56:11 -0300

    Dto, ViewModel e Mapper

---
+++ Rentfy.Application/DTO/StoreDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Rentfy.Application.DTO
+{
+    public class StoreDto
+    {
+        public long Id { get; set; }
+        public long LegalPersonId { get; set; }
+    }
+}
---
+++ Rentfy.Application/ViewModels/StoreViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace Rentfy.Application.ViewModels
+{
+    public class StoreViewModel
+    {
+        public long Id { get; set; }
+        public IEnumerable<ProductViewModel> Products { get; set; }
+        public LegalPersonViewModel LegalPerson { get; set; }
+    }
+}
