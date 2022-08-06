609de9b67f2830eaadc10fa6906905ef965f4867
Gilberto Barbosa <gilbertoaraujo229@gmail.com>
2022-08-06 14:21:11 -0300

    AEnumeration config

---
+++ Rentfy.Data/Mapping/AEnumerationMapping.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Rentfy.Domain.SeedWork;
+
+namespace Rentfy.Data.Mapping
+{
+    public abstract class AEnumerationMapping<TEnumeration> : IEntityTypeConfiguration<TEnumeration>
+            where TEnumeration : AEnumeration
+    {
+        public void Configure(EntityTypeBuilder<TEnumeration> builder)
+        {
+            builder.ToTable(typeof(TEnumeration).Name.Replace("Enumeration", string.Empty));
+
+            builder.HasKey(e => e.Id);
+
+            builder.Property(e => e.Id)
+               .ValueGeneratedNever()
+               .IsRequired();
+
+            ConfigureProprerties(builder);
+        }
+
+        protected virtual void ConfigureProprerties(EntityTypeBuilder<TEnumeration> builder)
+        {
+            builder.Property(t => t.Name)
+                .HasColumnName("Name")
+                .HasMaxLength(80)
+                .IsRequired();
+        }
+    }
+}
---
+++ Rentfy.Domain/SeedWork/AEnumeration.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace Rentfy.Domain.SeedWork
+{
+    public abstract class AEnumeration<TEnum> : AEnumeration
+            where TEnum : AEnumeration
+    {
+        protected AEnumeration(int id, string name) : base(id, name) { }
+
+        public static IEnumerable<TEnum> GetAll()
+            => GetEnumerations<TEnum>();
+
+        public static TEnum FromName(string name)
+            => GetEnumerations<TEnum>().FirstOrDefault(e => e.Name.ToLower() == name.ToLower());
+
+        public static TEnum FromId(int id)
+            => GetEnumerations<TEnum>().FirstOrDefault(e => e.Id == id);
+    }
+
+    public abstract class AEnumeration : IComparable
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        protected AEnumeration(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        protected static IEnumerable<T> GetEnumerations<T>()
+            where T : AEnumeration
+        {
+            var fields = typeof(T).GetFields(BindingFlags.Public | BindingFlags.Static | BindingFlags.DeclaredOnly)
+                .Where(x => !x.IsLiteral);
+            return fields.Select(f => f.GetValue(null)).Cast<T>();
+        }
+
+        public override bool Equals(object otherObj)
+        {
+            var otherValue = otherObj as AEnumeration;
+
+            if (otherValue == null) return false;
+
+            var typeMatches = GetType().Equals(otherObj.GetType());
+            var valueMatches = Id.Equals(otherValue.Id);
+
+            return typeMatches && valueMatches;
+        }
+
+        public override int GetHashCode() => Id.GetHashCode();
+
+        public override string ToString() => Name;
+
+        public int CompareTo(object otherObj) => Id.CompareTo(((AEnumeration)otherObj).Id);
+
+    }
+}
